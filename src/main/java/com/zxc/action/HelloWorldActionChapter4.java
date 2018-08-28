4a1fe2a358bc2bc54ffb1507ebc647203d8df0ce
ZXC <496461440@qq.com>
2018-08-28 22:11:14 +0800

    2

---
+++ src/main/java/com/zxc/action/HelloWorldActionChapter4.java
@@ -0,0 +1,39 @@
+package com.zxc.action;
+
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.AbstractController;
+import org.springframework.web.servlet.mvc.LastModified;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+//@Controller
+//@RequestMapping("/chapter3")
+@Component("/lastModified")
+public class HelloWorldActionChapter4 extends AbstractController implements LastModified{
+
+    private long lastModified;
+
+    @RequestMapping("/hello")
+    public String getHelloContent(String name) {
+        return "index";
+    }
+
+    @Override
+    public long getLastModified(HttpServletRequest httpServletRequest) {
+        if (lastModified == 0L)
+            lastModified = System.currentTimeMillis();
+        return lastModified;
+    }
+
+    @Override
+    protected ModelAndView handleRequestInternal(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) throws Exception {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("index");
+        return modelAndView;
+    }
+}
